7d597c9e977048580bc51e867d6ef58005a1fb53
Sanjana Kumari <sanjana01999@gmail.com>
2020-10-30 23:49:57 +0530

    port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
